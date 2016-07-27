e224a880406011bd676b4ba705da6bf6e0d10f11
Ayachi Gin <ayachigin@gmail.com>
2016-07-27 21:35:10 +0900

    echo

--- dll_main.d
+++ dll_main.d
@@ -9,76 +9,67 @@ import std.regex;
 import std.typecons;
 import std.windows.charset;
 
-import saori: Request, Response, parseRequest, fromMBS;
+import makoto: Request, Response, parseRequest, fromMBS;
 
-// SAORIがロードされたときに渡されるパスをいれとく変数。
 string working_directory;
 
 // dll作るときの定型文
 version(Windows) extern(Windows) bool DllMain(void* hInstance,
-	                                          uint ulReason,
+                                              uint ulReason,
                                               void*)
 {
-	switch (ulReason)
-	{
-		default: assert(0);
-		case DLL_PROCESS_ATTACH:
-			dll_process_attach( hInstance, true );
-			break;
-			
-		case DLL_PROCESS_DETACH:
-			dll_process_detach( hInstance, true );
-			break;
-			
-		case DLL_THREAD_ATTACH:
-			dll_thread_attach( true, true );
-			break;
-			
-		case DLL_THREAD_DETACH:
-			unload();
-			dll_thread_detach( true, true );
-			break;
-	}
-	return true;
+    switch (ulReason)
+    {
+    default: assert(0);
+    case DLL_PROCESS_ATTACH:
+        dll_process_attach( hInstance, true );
+        break;
+
+    case DLL_PROCESS_DETACH:
+        dll_process_detach( hInstance, true );
+        break;
+
+    case DLL_THREAD_ATTACH:
+        dll_thread_attach( true, true );
+        break;
+
+    case DLL_THREAD_DETACH:
+        unload();
+        dll_thread_detach( true, true );
+        break;
+    }
+    return true;
 }
 
 
 // exportする関数たち
 extern(C) {
-	export bool load(HGLOBAL h, long n)
-	{
-		auto input = to!string(cast(char*)h);
-		working_directory = input;
-		GlobalFree(h);
-		return true;
-	}
-	
-	export bool unload()
-	{
-		return true;
-	}
-	
-	export HGLOBAL request(HGLOBAL h, long *n)
-	{
-		// 入力を受け取る
-		//auto request = parseRequest(to!string(cast(char*)h));
-		auto request = parseRequest(fromMBS(h));
-		GlobalFree(h);
-		string res;
-		switch (request.command) {
-			case "GET Version":
-				res = new Response([]).toResponseString;
-				break;
-			case "EXECUTE":
-				res = new Response(request.arguments).toResponseString;
-				break;
-			default:
-				break;
-		}
+    export bool load(HGLOBAL h, long n)
+    {
+        auto input = to!string(cast(char*)h);
+        working_directory = input;
+        GlobalFree(h);
+        return true;
+    }
 
-		auto len = res.length;
-		h = LocalAlloc(0, len+1);
-		cstring.memcpy(h, toMBSz(res), len+1);
-		return h;
-	}
+    export bool unload()
+    {
+        return true;
+    }
+
+    export HGLOBAL request(HGLOBAL h, long *n)
+    {
+        // 入力を受け取る
+        //auto request = parseRequest(to!string(cast(char*)h));
+        auto request = parseRequest(fromMBS(h));
+        GlobalFree(h);
+        string res;
+
+        res = to!string(new Response(request.str));
+
+        auto len = res.length;
+        h = LocalAlloc(0, len+1);
+        cstring.memcpy(h, toMBSz(res), len+1);
+        return h;
+    }
 }
---
+++ makoto.d
@@ -0,0 +1,78 @@
+﻿module makoto;
+
+import std.conv: to;
+import std.stdio;
+import std.regex;
+import std.typecons: Tuple;
+import std.c.windows.windows: HGLOBAL, GlobalFree;
+import std.windows.charset;
+import std.stdio;
+
+alias Tuple!(string, "command",
+             string, "charset",
+             string, "str",
+             string, "sender") Request;
+
+Request parseRequest(string input)
+{
+    auto command_regex   = regex(r"^(?P<command>EXECUTE|GET Version)");
+    auto charset_regex   = regex(r"Charset: (?P<charset>.+)\r\n");
+    auto string_regex    = regex(r"String: (?P<str>[^\r\n]+)");
+    auto sender_regex    = regex(r"Sender: (?P<sender>.+)\r\n");
+
+    string command = input.matchFirst(command_regex).captures["command"];
+    string charset = input.matchFirst(charset_regex).captures["charset"];
+    string str     = input.matchFirst(string_regex).captures["str"];
+    string sender  = input.matchFirst(sender_regex).captures["sender"];
+
+    return Request(command, charset, str, sender);
+}
+
+unittest {
+    auto r = parseRequest("EXECUTE MAKOTO/2.0\r\n" ~
+                          "Sender: embryo\r\n" ~
+                          "Charset: Shift_JIS\r\n" ~
+                          "String: Hello\r\n");
+    assert(r.command == "EXECUTE");
+    assert(r.sender == "embryo");
+    assert(r.charset == "Shift_JIS");
+    assert(r.str == "Hello");
+}
+
+string fromMBS(HGLOBAL h)
+{
+    auto s = fromMBSz(cast (immutable(char)*) to!string(cast (char*) h));
+    GlobalFree(h);
+    return s;
+}
+
+class Response
+{
+    private string status;
+    private string charset;
+    private string str;
+
+    this(string str,
+         string status="200 OK",
+         string charset="Shift_JIS")
+    {
+        this.str = str;
+        this.status = status;
+        this.charset = charset;
+    }
+
+    override string toString()
+    {
+        string res;
+        res = "MAKOTO/2.0 " ~ this.status;
+        res ~= "\r\nCharset: " ~ this.charset;
+        res ~= "\r\nString: " ~ this.str;
+        res ~= "\r\n";
+        return res;
+    }
+    unittest {
+        auto res = new Response("hoge");
+        auto res_str = to!string(res);
+        assert(res_str == "MAKOTO/2.0 200 OK\r\nCharset: Shift_JIS\r\nString: hoge\r\n");
+    }
+}
